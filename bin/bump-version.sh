5975eae988204d38fa3cad2fd2b30aeef20d8b6d
Yuki Takei <yuki@weseek.co.jp>
2017-06-09 01:07:01 +0900

    ensure to be able to update README.md with `bin/bump-version.sh`

--- bin/bump-version.sh
+++ bin/bump-version.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
 sed -i -e "s/\(ENV APP_VERSION\) \(v.\+\)/\1 ${NEW_APP_VERSION}/" Dockerfile
+sed -i -e "s/^\([*] \[\`\)[^\`]\+\(\`, \`1\.2\`, .\+\]\)\(.\+\/blob\/v\).\+\(\/Dockerfile.\+\)$/\1${NEW_APP_VERSION}\2\3${NEW_APP_VERSION}\4/" README.md
